ff11e7019f09ad25b3d0e02160101a1e8eabf3b7
SSStuart <ssstuart.glunabouli@gmail.com>
2025-09-29 13:53:37 +0200

    Added arrested suspect behavior
    - Added a settings to define qhat should happend when the currently watched suspect is arrested/killed : Don't do anything (keep watching), stop the camera or switch to remaining suspects.
    - Improved detection of end of pursuit/suspect arrested... to Stop or Switch.
    - Moved settings loading log to IniFile.cs.
    - Renamed main loop.

--- IniFile.cs
+++ IniFile.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using Rage;
+using System.Text.RegularExpressions;
 
 namespace HeliView
 {
@@ -9,18 +10,32 @@ namespace HeliView
         internal static bool EnableOverlay = true;
         internal static bool WarpPlayerInHeli = true;
         internal static string HeliType = "cop";
+        internal static string onPedArrestBehavior = "1";
 
         internal static string path = "Plugins/LSPDFR/HeliView.ini";
         internal static InitializationFile ini = new InitializationFile(path);
 
         internal static void LoadSettings()
         {
-            Game.LogTrivial("[LOG]: Loading config file for HeliView.");
+            Game.LogTrivial("[HeliView]: Loading config file:");
 
             ini.Create();
             EnableOverlay = ini.ReadBoolean("Settings", "EnableOverlay", true);
+            Game.LogTrivial($"Enable Overlay: {(EnableOverlay ? "Yes" : "No")}");
             WarpPlayerInHeli = ini.ReadBoolean("Settings", "WarpPlayerInHeli", true);
+            Game.LogTrivial($"Warp player in Heli: {(WarpPlayerInHeli ? "Yes" : "No")}");
             HeliType = ini.ReadString("Settings", "HeliType", "cop");
+            Game.LogTrivial($"Heli type: {HeliType}");
+            onPedArrestBehavior = ini.ReadString("Settings", "onPedArrestBehavior", "1");
+            if (Regex.IsMatch(onPedArrestBehavior, @"^0|1|2\+(0|1)$"))
+            {
+                Game.LogTrivial($"On ped arrest behavior: {(onPedArrestBehavior == "0" ? "Do nothing" : (onPedArrestBehavior == "1" ? "Stop pursuit camera" : (onPedArrestBehavior == "2+0" ? "Switch remaining suspect or Do nothing" : "Switch remaining suspect or Stop pursuit camera")))}");
+            }
+            else
+            {
+                onPedArrestBehavior = "1";
+                Game.LogTrivial($"Invalid setting for onPedArrestBehavior: '{onPedArrestBehavior}'. Using default value '1' (Stop pursuit camera).");
+            }
         }
 
         internal static void SaveSetting(string setting, bool value)
